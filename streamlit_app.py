d5c4b58bb6c6036f6c4749cc215850a38acfa4f3
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-16 11:18:18 +0530

    Update streamlit_app.py
    added new variable to listed the data 
    and then located from main dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 # choose few fruit to set an example 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
