fabad037983592aacc5a3b42f44f7492c3a9e746
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-16 10:51:40 +0530

    Update streamlit_app.py
    correct spell mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('breakfas menu')
+streamlit.header('breakfast menu')
 streamlit.text('omega 3 & blueberry oatmeal')
 streamlit.text('kale,spinach & rocket smoothoie')
