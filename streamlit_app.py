591566026c1474ed3d5c735591024344f9c97ae7
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-17 16:18:14 +0530

    Update streamlit_app.py
    new session added to display fruity vise api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,10 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]      # it will gives only avocado and strawberries data
 streamlit.dataframe(fruits_to_show)
+
+
+# api with streamlit
+# new session to display fruity_vise_API response
+import requests
+fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruity_vise_response)
