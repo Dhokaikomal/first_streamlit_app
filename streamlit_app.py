4cbea0a2147675e0179d30bdbba73d2ce0b8259f
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-17 17:50:25 +0530

    Update streamlit_app.py
    added one header which shows which  fruit  is selected by customer
    and according to choice it shows data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,10 @@ fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_normalized = pandas.json_normalize(fruity_vise_response.json())
 # create a data frame
 streamlit.dataframe(fruityvice_normalized)
+
+
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
