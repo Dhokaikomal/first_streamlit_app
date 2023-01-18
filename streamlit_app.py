9b31d98537c733d879881e20ea043e5cd91ae523
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-18 15:09:12 +0530

    Update streamlit_app.py
    added try with if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,16 +47,32 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(fruityvice_normalized)
 
 
+
+
+
+
+# new section to display fruityvice api response 
+
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+        streamlit.error('please select one fruit ')
+  else:
+        fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruity_vise_response.json())
+        # create a data frame
+        streamlit.dataframe(fruityvice_normalized)
+
+      
+except URLError as e:
+  streamlit.eerror()
+
+#streamlit.write('The user entered ', fruit_choice)
 
 
-fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-fruityvice_normalized = pandas.json_normalize(fruity_vise_response.json())
-# create a data frame
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 
