9516d49d0b7461191d06a95e2fa2faacf696bcff
Dhokai komal <109612945+Dhokaikomal@users.noreply.github.com>
2023-01-18 15:20:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 fruity_vise_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruity_vise_response.json())  
         # create a data frame
-        streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
       
 
